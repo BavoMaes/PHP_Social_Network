0df60f5c2a45d6f688ccbc89480b2dbc4488b53f
Bavo <bavo@Bavos-MacBook-Pro.local>
2017-08-13 20:58:55 +0200

    Registration form HTML

---
+++ index.php
@@ -0,0 +1,41 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Register</title>
+
+  <!-- <link rel="stylesheet" href="style.css"> 
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400" rel="stylesheet"> -->
+
+</head>
+
+<body>
+    <form>
+  <h3>First name:</h3><br>
+  <input type="text" name="firstname"><br>
+  <h3>Last name:</h3><br>
+  <input type="text" name="lastname"><br>
+        <h3>Username:</h3><br>
+  <input type="text" name="username"><br>
+        <h3>Password:</h3><br>
+  <input type="password" name="password"><br>
+        <h3>Password again:</h3><br>
+  <input type="password" name="password2"><br>
+        <h3>Date of Birth:</h3><br>
+        Day (1-31):
+  <input type="text" name="day"><br>
+        Month (1-12):
+  <input type="text" name="month"><br>
+        Year:
+  <input type="text" name="year"><br>
+        <h3>Gender:</h3><br>
+          <input type="radio" name="gender" value="male" checked> Male<br>
+  <input type="radio" name="gender" value="female"> Female<br>
+  <input type="radio" name="gender" value="other"> Other<br>
+        <input type="submit" value="Submit">
+</form>
+  <!-- <script src="js/scripts.js"></script> -->
+</body>
+</html>
