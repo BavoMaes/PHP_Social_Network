64fae4dc207eca24f1761f44bfdcf943620fcdaa
Bavo <bavo@Bavos-MacBook-Pro.local>
2017-08-14 00:29:19 +0200

    Standard form

--- index.php
+++ index.php
@@ -6,31 +6,31 @@
 
   <title>Register</title>
 
-  <!-- <link rel="stylesheet" href="style.css"> 
-<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400" rel="stylesheet"> -->
+<link rel="stylesheet" href="style.css"> 
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400" rel="stylesheet">
 
 </head>
 
 <body>
     <form>
-  <h3>First name:</h3><br>
+  First name:<br>
   <input type="text" name="firstname"><br>
-  <h3>Last name:</h3><br>
+  Last name:<br>
   <input type="text" name="lastname"><br>
-        <h3>Username:</h3><br>
+        Username:<br>
   <input type="text" name="username"><br>
-        <h3>Password:</h3><br>
+        Password:<br>
   <input type="password" name="password"><br>
-        <h3>Password again:</h3><br>
+        Password again:<br>
   <input type="password" name="password2"><br>
-        <h3>Date of Birth:</h3><br>
+        Date of Birth:<br>
         Day (1-31):
   <input type="text" name="day"><br>
         Month (1-12):
   <input type="text" name="month"><br>
         Year:
   <input type="text" name="year"><br>
-        <h3>Gender:</h3><br>
+        Gender:<br>
           <input type="radio" name="gender" value="male" checked> Male<br>
   <input type="radio" name="gender" value="female"> Female<br>
   <input type="radio" name="gender" value="other"> Other<br>
